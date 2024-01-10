bbafc1baa3eee7761d51fd616426f3d3664cacf4
taffydesu <taffyvibritannia@gmail.com>
2024-01-10 16:01:53 +0900

    added the search engine

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-alert("Hello World!");
+function citySearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = searchInput.value;
+  cityElement = cityElement.trim();
+  cityElement = cityElement.toUpperCase();
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", citySearchSubmit);
