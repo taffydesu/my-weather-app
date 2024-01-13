3f35b70766cdab38c7b65aab91fa86f0ce6eaa0e
taffydesu <taffyvibritannia@gmail.com>
2024-01-13 18:27:36 +0900

    added axios, api and search functionality

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city-name");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "b800f3t5c5a32643644fbd333b098o7b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function citySearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city-name");
-  cityElement.innerHTML = searchInput.value;
-  cityElement = cityElement.trim();
-  cityElement = cityElement.toUpperCase();
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", citySearchSubmit);
+
+searchCity("Tokyo");
